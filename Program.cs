780c0b03a4e6ce610e650043b58b7b7a0bf9d2db
Kleiton MQ <kleiton.x5@gmail.com>
2022-05-18 19:17:54 -0300

    assistido até a penultima aula dos array

--- Program.cs
+++ Program.cs
@@ -1,28 +1,61 @@
 ﻿using static System.Console;
 using CsharpColections.helper;
 
-int[] lista = new int[10]{6, 18, 100, 50, 10, 1, 95, 15, 45, 20};
-int[] copiaLista = new int[15];
-OprecacoesArray organizar = new OprecacoesArray();
 
-WriteLine("Lista desorganizada");
-organizar.ImprimirArray(lista);
+static void AulaOrganizacaoComparacao()
+{
 
-WriteLine("Lista organizada.");
-//organizar.OrdenarBubleSort(ref lista);
-organizar.CopiarArray(ref lista, ref copiaLista);
-organizar.Ordenar(ref lista);
-organizar.ImprimirArray(lista);
-WriteLine("Lista Copiada antes da organização");
-organizar.ImprimirArray(copiaLista);
+    int[] lista = new int[10] { 6, 18, 100, 50, 10, 1, 95, 15, 45, 20 };
+    int[] copiaLista = new int[15];
+    OprecacoesArray organizar = new OprecacoesArray();
 
-bool existe = organizar.VerificarExistencia(lista, 150);
+    WriteLine("Lista desorganizada");
+    organizar.ImprimirArray(lista);
 
-if (existe){
-    WriteLine("valor procurado encontrado");
-}else{
-    WriteLine("Valor procurado não encontrado.");
+    WriteLine("Lista organizada.");
+    //organizar.OrdenarBubleSort(ref lista);
+    organizar.CopiarArray(ref lista, ref copiaLista);
+    organizar.Ordenar(ref lista);
+    organizar.ImprimirArray(lista);
+    WriteLine("Lista Copiada antes da organização");
+    organizar.ImprimirArray(copiaLista);
+
+    bool existe = organizar.VerificarExistencia(lista, 150);
+
+    if (existe)
+    {
+        WriteLine("valor procurado encontrado");
+    }
+    else
+    {
+        WriteLine("Valor procurado não encontrado.");
+    }
+
+    bool todosMaiorQue = organizar.TodosMaiorQue(lista, 0);
+    WriteLine(todosMaiorQue);
 }
 
-bool todosMaiorQue = organizar.TodosMaiorQue(lista, 0);
-WriteLine(todosMaiorQue);
+int[] lista = new int[10] { 6, 0, 100, 18, 10, 1, 95, 15, 45, 20 };
+    int[] copiaLista = new int[15];
+    OprecacoesArray organizar = new OprecacoesArray();
+
+    int valorProcurado = 20;
+
+    int valorAchado = organizar.ObterValor(lista, valorProcurado);
+    if (valorAchado > 0)
+    {
+        WriteLine("encontrei o valor");
+    }else{
+        WriteLine("NÃO encontrei o valor");
+    }
+
+    int index = organizar.ObterIndice(lista, valorProcurado);
+    WriteLine(index);
+
+    index = organizar.ObterIndice2(lista, valorProcurado);
+    WriteLine(index);
+
+WriteLine($"Capacidade atual do array {lista.Length}");
+organizar.RedimensionarArray(ref lista, lista.Length*2);
+
+WriteLine($"Capacidade após redimensionamento é {lista.Length}");
--- helper/OprecacoesArray.cs
+++ helper/OprecacoesArray.cs
@@ -43,8 +43,26 @@ namespace CsharpColections.helper
             return Array.Exists(array, elemento => elemento == valor);
         }
 
-        public bool TodosMaiorQue(int[] array, int valor){
+        public bool TodosMaiorQue(int[] array, int valor)
+        {
             return Array.TrueForAll(array, elemento => elemento > valor);
+        }
+
+        public int ObterValor(int[] array, int valor)
+        {
+            return Array.Find(array, elemento => elemento == valor);
+        }
+
+        public int ObterIndice(int[] array, int valor)
+        {
+            return Array.FindIndex(array, elemento => elemento == valor);
+        }
+        public int ObterIndice2(int[] array, int valor)
+        {
+            return Array.IndexOf(array, valor);
+        }
+        public void RedimensionarArray(ref int[] array, int novoTamanho){
+          Array.Resize(ref array, novoTamanho);   
         }
     }
 }
